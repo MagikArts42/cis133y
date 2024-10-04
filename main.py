8b6e43addc35b55bfccd22284959ecb863f11513
MagikArts42 <jeff@magikarts.com>
2024-10-03 20:27:42 -0700

    changes to use output module

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-# This is a sample Python script.
+import output as o
+def main():
+    name = input("Input your name: ")
+    o.output("Hello, " + name)  # this is a stub to be replaced with call to collaborator's function.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
---
+++ output.py
@@ -0,0 +1,2 @@
+def output(data):
+    print(data)
